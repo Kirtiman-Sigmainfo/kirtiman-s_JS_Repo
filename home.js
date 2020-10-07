eab690ef2a82909beab4c4cedd2b20b00c6b7f94
Kirtiman-Sigmainfo <kirtiman.dutta@sigmainfo.net>
2020-10-07 21:45:34 +0530

    First commit

---
+++ home.js
@@ -0,0 +1,27 @@
+console.log("Hello!");
+//alert("This is kirtiman")
+
+//var p = "Smoothie";
+//alert(p);
+
+
+
+var name=prompt("what is your name?");
+
+document.getElementById('sometext').innerHTML="Hey there! "+ name +"Please open console";
+
+function fun(){
+    alert("say hi to function and lets have fun");
+}
+
+fun();
+let fruits =['apple','mango','guava'];
+console.log(fruits.join('_'));
+
+let students={
+    name:'kirtiman',
+    class:'employed',
+    age:23
+};
+
+console.log(students.age);
